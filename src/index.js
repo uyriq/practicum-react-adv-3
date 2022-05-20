428a572cc89fa2310d9f3f7f6cb1fde282783bf7
uyriq <105006434+uyriq@users.noreply.github.com>
2022-05-20 23:58:54 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import ReactDOM from 'react-dom';
 import "./index.css";
 
 import App from "./components/app/app";
